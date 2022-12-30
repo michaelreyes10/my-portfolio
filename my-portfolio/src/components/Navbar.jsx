d2b875b880a41f00fd926369f88d9fed57c9b1d6
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-12-30 11:59:45 -0500

    navbar hamburger functionality

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import Logo from '../assets/logo.png'
 
 export const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
@@ -19,20 +21,18 @@ export const Navbar = () => {
                 <li>Contact</li>
             </ul>
         {/*Hamburger*/}
-        <div className='md:hidden'>
-            <FaBars/>
+        <div onClick={handleClick}className='md:hidden z-10'>
+            {!nav ? <FaBars/> : <FaTimes/>}
         </div>
-        {/*Mobile Menu*/}
-        <div className='hidden'>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Expirience</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+        {/*Mobile Menu*/}   
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Expirience</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Work</li>
+                <li className='py-6 text-4xl'>Contact</li>
             </ul>
-        </div>
         {/*Social Icons*/}
         <div className='hidden'>
         </div>
